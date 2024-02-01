a6fadcf3f2a762738d85157fc64db9a17009c92c
1NASEM <120907387+1NASEM@users.noreply.github.com>
2024-02-01 18:01:51 +0300

    Add project files.

---
+++ Guess The Number Game 2/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Guess_The_Number_Game_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputNumber = new System.Windows.Forms.TextBox();
+            this.newGameBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(21, 27);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(282, 17);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "I have a number between 1 and 1000.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Can you guess the number?";
+            // 
+            // inputNumber
+            // 
+            this.inputNumber.Location = new System.Drawing.Point(90, 105);
+            this.inputNumber.Name = "inputNumber";
+            this.inputNumber.Size = new System.Drawing.Size(100, 20);
+            this.inputNumber.TabIndex = 2;
+            this.inputNumber.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inputNumber_KeyDown);
+            // 
+            // newGameBtn
+            // 
+            this.newGameBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newGameBtn.Location = new System.Drawing.Point(71, 131);
+            this.newGameBtn.Name = "newGameBtn";
+            this.newGameBtn.Size = new System.Drawing.Size(136, 51);
+            this.newGameBtn.TabIndex = 3;
+            this.newGameBtn.Text = "Start a new game";
+            this.newGameBtn.UseVisualStyleBackColor = true;
+            this.newGameBtn.Click += new System.EventHandler(this.newGameBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.newGameBtn);
+            this.Controls.Add(this.inputNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Guess The Number Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputNumber;
+        private System.Windows.Forms.Button newGameBtn;
+    }
+}
+
---
+++ Guess The Number Game 2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guess_The_Number_Game_2
+{
+    public partial class Form1 : Form
+    {
+       Random randomnum=new Random();
+        int num = 0;
+        int diff = 0;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            LoadQuestions();
+
+        }
+
+
+        
+
+        private void inputNumber_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                inputNumber.Focus();
+                int input = Convert.ToInt32(inputNumber.Text);
+                
+                if (input == num)
+                {
+                    this.BackColor = Color.LightGray;
+                    MessageBox.Show("Correct Guess");
+                    inputNumber.Text = "";
+                    
+                    
+                }
+                else if (input< num )
+                {
+                    this.BackColor = Color.Blue;
+                    MessageBox.Show("Too low , Try a Higher number");
+                    
+                }
+                else
+                {
+                    this.BackColor = Color.Red;
+                    MessageBox.Show("Too high, Try a lower number");
+                    
+                }
+                
+            }
+
+        }
+        private void LoadQuestions()
+        {
+            num = randomnum.Next(1, 1001);
+            lbl1.Text = "I am thinking of a number between 1 and 1000";
+            
+        }
+
+        private void newGameBtn_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+        private void NewGame()
+        {
+            num = randomnum.Next(1, 1001);
+            newGameBtn.Enabled = true;
+            newGameBtn.BackColor = Color.White;
+            inputNumber.Clear();
+            
+            this.BackColor = Color.White;
+        }
+    }
+}
